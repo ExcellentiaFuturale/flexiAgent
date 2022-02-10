e96b5aea3a77279e4d98afb73ff2f418997a7651
Denys Haryachyy <denysh@flexiwan.com>
2022-02-10 21:40:51 +0000

    PPPoE: Do not run thread during VPP starting/stopping phases.

--- fwpppoe.py
+++ fwpppoe.py
@@ -507,7 +507,8 @@ class FwPppoeClient(FwObject):
             time.sleep(1)
 
             try:  # Ensure thread doesn't exit on exception
-                self.scan()
+                if not fwglobals.g.router_api.state_is_starting_stopping():
+                    self.scan()
             except Exception as e:
                 self.log.error("%s: %s (%s)" %
                     (threading.current_thread().getName(), str(e), traceback.format_exc()))
