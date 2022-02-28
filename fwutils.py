96883fbc3228d9570070cf754a178ad429f6aa86
Denys Haryachyy <denysh@flexiwan.com>
2022-02-28 11:21:33 +0000

    PPPoE : Fix WAN monitor for PPPoE interface

--- fwutils.py
+++ fwutils.py
@@ -719,6 +719,9 @@ def build_interface_dev_id(linux_dev_name, sys_class_net=None):
     if not linux_dev_name:
         return ""
 
+    if linux_dev_name.startswith('ppp'):
+        return fwglobals.g.pppoe.pppoe_get_dev_id_from_ppp(linux_dev_name)
+
     if sys_class_net is None:
         cmd = "sudo ls -l /sys/class/net"
         try:
@@ -1973,10 +1976,6 @@ def is_interface_without_dev_id(if_name):
     if if_name.startswith('br_'):
         return True
 
-    # PPPoE interface has no dev_id
-    if if_name.startswith('ppp'):
-        return True
-
     return False
 
 def get_lte_interfaces_names():
@@ -3179,15 +3178,18 @@ def dump(filename=None, path=None, clean_log=False):
     except Exception as e:
         fwglobals.log.error("failed to dump: %s" % (str(e)))
 
-def linux_check_gateway_exist(gw):
+def linux_check_gateway_exist(gw, check_subnet=True):
     interfaces = psutil.net_if_addrs()
     net_if_stats = psutil.net_if_stats()
+
     for if_name in interfaces:
         addresses = interfaces[if_name]
         for address in addresses:
             if address.family == socket.AF_INET:
                 network = IPNetwork(address.address + '/' + address.netmask)
-                if net_if_stats[if_name].isup and is_ip_in_subnet(gw, str(network)):
+                if check_subnet and not is_ip_in_subnet(gw, str(network)):
+                    return False
+                if net_if_stats[if_name].isup:
                     return True
 
     return False
