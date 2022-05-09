ExcellentiaFuturale/flexiAgent

commit 1054b8a08713c25ad69cf4b338fe8a1427126a59

Implement https://gitlab.com/flexiwangroup/flexiagent/-/issues/868 - revert native Thread (issue 709) for STUN to avoid blocking WebSocket connection by STUN activity

fwstun_wrapper.py
@@ -6,7 +6,6 @@ import socket
 import psutil
 import fwglobals
 import fwtunnel_stats
-import fwthread
 import fwutils
 import time
 import traceback
@@ -72,12 +71,6 @@ class FwStunWrap(FwObject):
         self.stun_retry    = 60
         fwstun.set_log(fwglobals.log)
 
-        self.thread_reset_all_timeout = 10 * 60
-        self.thread_update_cache_from_os_timeout = 2 * 60
-        self.thread_send_stun_timeout = 3
-        self.thread_probe_sym_nat_timeout = 30
-        self.thread_send_sym_nat_timeout = 3
-
     def _log_address_cache(self):
         """ prints the content on the local cache
         """
@@ -108,7 +101,7 @@ class FwStunWrap(FwObject):
             self._send_stun_requests()
             self._log_address_cache()
 
-        self.thread_stun = fwthread.FwRouterThread(target=self.stun_thread_func, name='STUN', log=self.log)
+        self.thread_stun = threading.Thread(target=self._stun_thread, name='STUN Thread')
         self.thread_stun.start()
 
     def finalize(self):
@@ -338,36 +331,57 @@ class FwStunWrap(FwObject):
 
         return nat_type, nat_ext_ip, nat_ext_port, stun_index
 
-    def stun_thread_func(self, ticks):
-        """STUN thread function.
-        It sends STUN requests for address:4789 in a timely manner
+    def _stun_thread(self, *args):
+        """STUN thread
+        Its function is to send STUN requests for address:4789 in a timely manner
         according to some algorithm-based calculations.
         """
-        # Don't STUN if vpp is being initializing / shutting down,
-        # as quering vpp for interface names/ip-s might generate exception.
-        if not fwglobals.g.router_api.state_is_starting_stopping():
+        self.log.debug(f"tid={fwutils.get_thread_tid()}: {threading.current_thread().name}")
 
-            # send STUN requests for addresses that a request was not sent for
-            # them, or for ones that did not get reply previously
-            if ticks % self.thread_send_stun_timeout == 0:
-                self._send_stun_requests()
+        slept = 1
+        reset_all_timeout = 10 * 60
+        update_cache_from_os_timeout = 2 * 60
+        send_stun_timeout = 3
+        probe_sym_nat_timeout = 30
+        send_sym_nat_timeout = 3
 
-            # probe tunnels in down state to see if we could find remote edge
-            # address/port from incoming packets for symmetric NAT traversal
+        while not fwglobals.g.teardown:
 
-            if ticks % self.thread_send_sym_nat_timeout == 0:
-                self._send_symmetric_nat()
+            try:  # Ensure thread doesn't exit on exception
 
-            if ticks % self.thread_probe_sym_nat_timeout == 0:
-                self._probe_symmetric_nat()
+                # Don't STUN if vpp is being initializing / shutting down,
+                # as quering vpp for interface names/ip-s might generate exception.
+                if not fwglobals.g.router_api.state_is_starting_stopping():
 
-            if ticks % self.thread_reset_all_timeout == 0:
-                # reset all STUN information every 10 minutes
-                self._reset_all()
+                    # send STUN requests for addresses that a request was not sent for
+                    # them, or for ones that did not get reply previously
+                    if slept % send_stun_timeout == 0:
+                        self._send_stun_requests()
 
-            if ticks % self.thread_update_cache_from_os_timeout == 0:
-                # every update_cache_timeout, refresh cache with updated IP addresses from OS
-                self._update_cache_from_OS()
+                    # probe tunnels in down state to see if we could find remote edge
+                    # address/port from incoming packets for symmetric NAT traversal
+
+                    if slept % send_sym_nat_timeout == 0:
+                        self._send_symmetric_nat()
+
+                    if slept % probe_sym_nat_timeout == 0:
+                        self._probe_symmetric_nat()
+
+                    if slept % reset_all_timeout == 0:
+                        # reset all STUN information every 10 minutes
+                        self._reset_all()
+
+                    if slept % update_cache_from_os_timeout == 0:
+                        # every update_cache_timeout, refresh cache with updated IP addresses from OS
+                        self._update_cache_from_OS()
+
+            except Exception as e:
+                self.log.error("%s: %s (%s)" %
+                    (threading.current_thread().getName(), str(e), traceback.format_exc()))
+                pass
+
+            time.sleep(1)
+            slept += 1
 
     def handle_down_tunnels(self, tunnel_stats):
         """ Run over all tunnels and get the source IP address of the tunnels that are not connected.

fwglobals.py
@@ -294,6 +294,7 @@ class Fwglobals(FwObject):
         self.DUMP_FOLDER                   = '/var/log/flexiwan/fwdump'
         self.DEFAULT_DNS_SERVERS           = ['8.8.8.8', '8.8.4.4']
         self.router_threads                = FwRouterThreading() # Primitives used for synchronization of router configuration and monitoring threads
+        self.handle_request_lock           = threading.RLock()
 
         # Load configuration from file
         self.cfg = self.FwConfiguration(self.FWAGENT_CONF_FILE, self.DATA_PATH, log=log)
@@ -478,40 +479,41 @@ class Fwglobals(FwObject):
 
         :returns: Dictionary with error string and status code.
         """
-        try:
-            req    = request['message']
-            params = request.get('params')
+        with self.handle_request_lock:
+            try:
+                req    = request['message']
+                params = request.get('params')
 
-            handler      = request_handlers.get(req)
-            handler_func = getattr(self, handler.get('name'))
+                handler      = request_handlers.get(req)
+                handler_func = getattr(self, handler.get('name'))
 
-            reply = handler_func(request)
-            if reply['ok'] == 0:
-                vpp_trace_file = fwutils.build_timestamped_filename('',self.VPP_TRACE_FILE_EXT)
-                os.system('sudo vppctl api trace save %s' % (vpp_trace_file))
-                raise Exception(reply['message'])
+                reply = handler_func(request)
+                if reply['ok'] == 0:
+                    vpp_trace_file = fwutils.build_timestamped_filename('',self.VPP_TRACE_FILE_EXT)
+                    os.system('sudo vppctl api trace save %s' % (vpp_trace_file))
+                    raise Exception(reply['message'])
 
-            # On router configuration request, e.g. add-interface,
-            # remove-tunnel, etc. update the configuration database
-            # signature. This is needed to assists the database synchronization
-            # feature that keeps the configuration set by user on the flexiManage
-            # in sync with the one stored on the flexiEdge device.
-            # Note we update signature on configuration requests received from flexiManage only,
-            # but retrieve it into replies for all requests. This is to simplify
-            # flexiManage code.
-            #
-            if reply['ok'] == 1 and handler.get('sign') and received_msg:
-                fwutils.update_device_config_signature(received_msg)
-            reply['router-cfg-hash'] = fwutils.get_device_config_signature()
+                # On router configuration request, e.g. add-interface,
+                # remove-tunnel, etc. update the configuration database
+                # signature. This is needed to assists the database synchronization
+                # feature that keeps the configuration set by user on the flexiManage
+                # in sync with the one stored on the flexiEdge device.
+                # Note we update signature on configuration requests received from flexiManage only,
+                # but retrieve it into replies for all requests. This is to simplify
+                # flexiManage code.
+                #
+                if reply['ok'] == 1 and handler.get('sign') and received_msg:
+                    fwutils.update_device_config_signature(received_msg)
+                reply['router-cfg-hash'] = fwutils.get_device_config_signature()
 
-            return reply
+                return reply
 
-        except Exception as e:
-            global log
-            err_str = "%s(%s): %s" % (str(e), req, format(params))
-            log.error(err_str + ': %s' % str(traceback.format_exc()))
-            reply = {"message":err_str, 'ok':0}
-            return reply
+            except Exception as e:
+                global log
+                err_str = "%s(%s): %s" % (str(e), req, format(params))
+                log.error(err_str + ': %s' % str(traceback.format_exc()))
+                reply = {"message":err_str, 'ok':0}
+                return reply
 
     def _get_api_object_attr(self, api_type, attr):
         if api_type == '_call_router_api':

commit 2bf1b4eccac0ae3ce887a42f8c502c0d07239c89

Implement https://gitlab.com/flexiwangroup/flexiagent/-/issues/870 - fix exception in STUN thread introduced by 1054b8a087

fwstun_wrapper.py
@@ -345,7 +345,7 @@ class FwStunWrap(FwObject):
         probe_sym_nat_timeout = 30
         send_sym_nat_timeout = 3
 
-        while not fwglobals.g.teardown:
+        while not fwglobals.g.router_threads.teardown:
 
             try:  # Ensure thread doesn't exit on exception
 