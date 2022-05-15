a37c85479f4bf3df31495b5c945b9a4436b48b83
Shneor Pinsky <shneorp@flexiwan.com>
2022-05-15 10:10:40 +0000

    Revert fix in get_interface_address

--- fwutils.py
+++ fwutils.py
@@ -362,22 +362,20 @@ def get_all_interfaces():
 
     return dev_id_ip_gw
 
-def get_interface_address(if_name, if_dev_id=None, log=True, log_on_failure=None):
+def get_interface_address_by_dev_id(dev_id):
+    linux_if_name = dev_id_to_linux_if_name(dev_id)
+    return get_interface_address(linux_if_name)
+
+def get_interface_address(if_name, log=True, log_on_failure=None):
     """Gets IP address of interface by name found in OS.
 
     :param if_name:     Interface name.
-    :param if_dev_id:   Bus address of the interface, address for which is returned.
-                        If provided, the 'if_name' is ignored. The name is fetched
-                        from system by a Bus address.
     :param log:         If True the found/not found address will be logged.
                         Errors or debug info is printed in any case.
     :param log_on_failure: If provided, overrides the 'log' in case of not found address.
 
     :returns: IP address.
     """
-    if if_dev_id:
-        if_name = dev_id_to_tap(if_dev_id)
-
     if log_on_failure == None:
         log_on_failure = log
 
@@ -1524,8 +1522,6 @@ def reset_device_config(pppoe=False, applications=True):
      """
     with FwRouterCfg(fwglobals.g.ROUTER_CFG_FILE) as router_cfg:
         router_cfg.clean()
-    with FwRouterCfg(fwglobals.g.ROUTER_PENDING_CFG_FILE) as router_pending_cfg:
-        router_pending_cfg.clean()
     with FwSystemCfg(fwglobals.g.SYSTEM_CFG_FILE) as system_cfg:
         system_cfg.clean()
     if os.path.exists(fwglobals.g.ROUTER_STATE_FILE):
@@ -1563,8 +1559,6 @@ def reset_device_config(pppoe=False, applications=True):
 
     reset_router_api_db_sa_id() # sa_id-s are used in translations of router configuration, so clean them too.
     reset_router_api_db(enforce=True)
-
-    reset_device_config_signature("empty_cfg", log=False)
 
     restore_dhcpd_files()
 
@@ -1642,17 +1636,6 @@ def print_router_config(basic=True, full=False, multilink=False):
             cfg = router_cfg.dumps(full=full, types=['add-application','add-multilink-policy'])
         else:
             cfg = ''
-        print(cfg)
-
-def print_router_pending_config():
-    """Print router pending configuration - the configuration items that were
-    requested to be configured, but the configuration of that is not possible
-    at the moment. E.g. tunnels for interfaces without IP.
-
-     :returns: None.
-     """
-    with FwRouterCfg(fwglobals.g.ROUTER_PENDING_CFG_FILE) as router_pending_cfg:
-        cfg = router_pending_cfg.dumps()
         print(cfg)
 
 def print_general_database():
@@ -2270,8 +2253,7 @@ def vpp_multilink_update_labels(labels, remove, next_hop=None, dev_id=None, sw_i
         tap = vpp_if_name_to_tap(vpp_if_name)
         next_hop, _ = get_interface_gateway(tap)
     if not next_hop:
-        next_hop = "0.0.0.0"
-        fwglobals.log.warning(f"vpp_multilink_update_labels: no 'next_hop' was provided, use blackhole {next_hop}")
+        return (False, "'next_hop' was not provided and there is no default gateway")
 
     op = 'del' if remove else 'add'
 
@@ -3171,8 +3153,6 @@ def get_reconfig_hash():
 
         mtu = str(linux_interfaces[dev_id]['mtu'])
 
-        link = get_interface_link_state(name, dev_id)
-
         res += 'addr:'    + addr + ','
         res += 'gateway:' + gw + ','
         res += 'metric:'  + metric + ','
@@ -3180,7 +3160,6 @@ def get_reconfig_hash():
         if gw and addr:
             res += 'public_ip:'   + linux_interfaces[dev_id]['public_ip'] + ','
             res += 'public_port:' + str(linux_interfaces[dev_id]['public_port']) + ','
-        res += 'link:'  + link + ','
 
     hash = hashlib.md5(res.encode()).hexdigest()
     fwglobals.log.debug("get_reconfig_hash: %s: %s" % (hash, res))
