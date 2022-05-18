419c3af9387aad8d539ed2b2dd31458ca5465b0a
IgorN <igor@flexiwan.com>
2022-05-18 13:58:20 +0300

    Implement https://gitlab.com/flexiwangroup/flexiagent/-/issues/659 - don't fail 'start-router' on no-IP interfaces - wait a bit for DHCP IP - review fixes

--- fwnetplan.py
+++ fwnetplan.py
@@ -536,7 +536,7 @@ def add_remove_netplan_interface(is_add, dev_id, ip, gw, metric, dhcp, type, dns
         # For DHCP interfaces wait a bit - give a chance to system get IP
         #
         if dhcp and is_add:
-            for _ in range(3):
+            for _ in range(10):
                 time.sleep(1)
                 if fwutils.get_interface_address(if_name, log=False):
                     break
