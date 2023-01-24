7c0035aadeb5f4addd8d7c3d0129555ab8bd79fe
Igor Novoseltsev <igor@flexiwan.com>
2023-01-24 09:36:46 +0200

    fwdump.py: add apt logs

--- tools/fwdump.py
+++ tools/fwdump.py
@@ -54,6 +54,9 @@ g_dumpers = {
     ############################################################################
     # Linux stuff - !!! PLEASE KEEP ALPHABET ORDER !!!
     #
+    'linux_apt':                    { 'shell_cmd': 'mkdir -p <temp_folder>/linux_apt/ && ' +
+                                                   'cp /var/log/apt/* <temp_folder>/linux_apt 2>/dev/null ; ' +
+                                                   'true' },       # Add 'true' to avoid error status code returned by shell_cmd if file does not exists
     'linux_cpu':                    { 'shell_cmd': 'cat /proc/cpuinfo > <dumper_out_file>' },
     'linux_dhcpd':                  { 'shell_cmd': 'mkdir -p <temp_folder>/linux_dhcpd/ && ' +
                                                    'cp /etc/dhcp/dhcpd.conf* <temp_folder>/linux_dhcpd 2>/dev/null ; ' +
