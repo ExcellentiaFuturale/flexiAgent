c788784fe1652d21025ef1ce9d27bf42c4ae31e5
Shneor Pinsky <shneorp@flexiwan.com>
2023-02-14 11:58:09 +0000

    Add tunnel destination field

--- fwtranslate_add_tunnel.py
+++ fwtranslate_add_tunnel.py
@@ -776,7 +776,7 @@ def _add_vxlan_tunnel(cmd_list, cache_key, dev_id, bridge_id, src, dst, params):
             'src_address'          : src_addr,
             'dst_address'          : dst_addr,
             'vni'                  : bridge_id,
-            'dst_port'             : int(params.get('dstPort', source_vxlan_port)), # TODO: // Diego changed from "dest" to "dst". Take care on upgrade when vpp is starting with old translation.
+            'dest_port'            : int(params.get('dstPort', source_vxlan_port)),
             'src_port'             : int(params.get('srcPort', source_vxlan_port)),
             'substs': [{'add_param': 'next_hop_sw_if_index', 'val_by_func': 'dev_id_to_vpp_sw_if_index', 'arg': params['dev_id']},
                        {'add_param': 'next_hop_ip', 'val_by_func': 'get_tunnel_gateway', 'arg': [dst, dev_id]},
