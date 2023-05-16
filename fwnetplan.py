1d94406075cea8808d86f8f562155e86b4ce8235
Igor Novoseltsev <igor@flexiwan.com>
2023-05-16 12:25:30 +0300

    Fix https://gitlab.com/flexiwangroup/flexiagent/-/issues/1256 - 'start-router' fails when BRIDGE interface is configured

--- fwnetplan.py
+++ fwnetplan.py
@@ -572,7 +572,9 @@ def add_remove_netplan_interface(is_add, dev_id, ip, gw, metric, dhcp, type, dns
         # That covers static address case as well, that might require a second
         # or two for linux to update interfaces.
         #
-        if is_add and type != "TRUNK":
+        if is_add and \
+           not (type == "TRUNK") and \
+           not (not ip and type == "LAN" and dhcp != "yes"):    # bridge interfaces have no IP
             if_addr = fwutils.get_interface_address(ifname, log=False)
             if not if_addr and fwutils.vpp_get_interface_status(dev_id=dev_id).get('link') == "up":
                 for _ in range(10):
