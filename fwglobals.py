ExcellentiaFuturale/flexiAgent

commit 2c6f2e65d927116d7127ea3dfb637de062ae796b

Fix https://gitlab.com/flexiwangroup/flexiagent/-/issues/1369 - log truncated line in original non-truncated version to the dedicated agent.huge_lines.log file

fwglobals.py
@@ -296,6 +296,7 @@ class Fwglobals(FwObject):
         self.CONN_FAILURE_FILE   = self.DATA_PATH + '.upgrade_failed'
         self.IKEV2_FOLDER        = self.DATA_PATH + 'ikev2/'
         self.ROUTER_LOG_FILE     = '/var/log/flexiwan/agent.log'
+        self.HUGE_LINE_LOG_FILE  = '/var/log/flexiwan/agent.huge_lines.log'
         self.APPLICATION_IDS_LOG_FILE = '/var/log/flexiwan/application_ids.log'
         self.FIREWALL_LOG_FILE = '/var/log/flexiwan/firewall.log'
         self.AGENT_UI_LOG_FILE   = '/var/log/flexiwan/agentui.log'
@@ -361,6 +362,8 @@ class Fwglobals(FwObject):
 
         # Config limit for QoS scheduler memory usage (limits to 'x' % of configured VPP memory)
         self.QOS_SCHED_MAX_MEMORY_PERCENT = 5
+
+        log.set_huge_line_file(self.HUGE_LINE_LOG_FILE)
 
         # Load configuration from file
         self.cfg = self.FwConfiguration(self.FWAGENT_CONF_FILE, self.DATA_PATH, log=log)

fwlog.py
@@ -146,14 +146,21 @@ class Fwlog:
 
 
 class FwLogSyslog(Fwlog):
-    def __init__(self, level=FWLOG_LEVEL_INFO, identification="fwagent"):
+    def __init__(self, level=FWLOG_LEVEL_INFO, identification="fwagent", huge_line_file=None):
         """Constructor method
         """
         Fwlog.__init__(self, level=level, name="syslog(ident=fwagent)")
         syslog.openlog(ident=identification)
 
+        self.huge_line_log = None
+        if huge_line_file:
+            self.set_huge_line_file(huge_line_file)
+
     def __str__(self):
-        return "syslog"
+        return "syslog" if not self.huge_line_log else f"syslog & {str(self.huge_line_log)}"
+
+    def set_huge_line_file(self, huge_line_log_filename):
+        self.huge_line_log = FwLogFile(huge_line_log_filename)
 
     def _log(self, log_message, to_terminal=True, to_syslog=True, truncate_long_line=True):
         """Print log message.
@@ -170,7 +177,7 @@ class FwLogSyslog(Fwlog):
 
         if to_syslog and self.to_syslog_enabled:
 
-            chunk_len = 4096
+            chunk_len = 2048
 
             # Prepend prefix (name of class that produced log line) and truncate the log line to 4K.
             # Note syslog discards lines beyond 8K by default, so take a caution if you modify this code!
@@ -181,8 +188,10 @@ class FwLogSyslog(Fwlog):
                 return
 
             if truncate_long_line:
-                log_message = log_message[0:chunk_len] + ' <truncated>'
-                syslog.syslog(log_message)
+                truncated_message = log_message[0:chunk_len] + ' <truncated>'
+                syslog.syslog(truncated_message)
+                if self.huge_line_log:
+                    self.huge_line_log._log(log_message)
                 return
 
             msgs = [log_message[i:i+chunk_len] for i in range(0, len(log_message), chunk_len)]

tools/fwdump.py
@@ -167,6 +167,8 @@ g_dumpers = {
     'fwagent_logs': 				{ 'shell_cmd': 'mkdir -p <temp_folder>/logs && ' +
                                                    'cp /var/log/flexiwan/agent.log <temp_folder>/fwagent.log 2>/dev/null ;' + # save latest log into root folder for convenience
                                                    'cp /var/log/flexiwan/agent.log.1   <temp_folder>/logs/ 2>/dev/null ;' +
+                                                   'cp /var/log/flexiwan/agent.huge_lines.log   <temp_folder>/logs/ 2>/dev/null ;' +
+                                                   'cp /var/log/flexiwan/agent.huge_lines.log.1 <temp_folder>/logs/ 2>/dev/null ;' +
                                                    'cp /var/log/flexiwan/agentui.log   <temp_folder>/logs/ 2>/dev/null ;' +
                                                    'cp /var/log/flexiwan/agentui.log.1 <temp_folder>/logs/ 2>/dev/null ;' +
                                                    'true' },       # Add 'true' to avoid error status code returned by shell_cmd if file does not exists

commit c55a1a81545ecbbe8b094c4707d108ad6f301575

Fix https://gitlab.com/flexiwangroup/flexiagent/-/issues/1377 - exception in fwdump - bug introduced by 2c6f2e65d9 for 1369 - log non-truncated log lines to agent.huge_lines.log file

fwglobals.py
@@ -363,7 +363,8 @@ class Fwglobals(FwObject):
         # Config limit for QoS scheduler memory usage (limits to 'x' % of configured VPP memory)
         self.QOS_SCHED_MAX_MEMORY_PERCENT = 5
 
-        log.set_huge_line_file(self.HUGE_LINE_LOG_FILE)
+        if log:
+            log.set_huge_line_file(self.HUGE_LINE_LOG_FILE)
 
         # Load configuration from file
         self.cfg = self.FwConfiguration(self.FWAGENT_CONF_FILE, self.DATA_PATH, log=log)